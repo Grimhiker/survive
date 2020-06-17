dae8e6a171df3daaa3b41c642765e07a48da6be1
Grimhiker <66091618+Grimhiker@users.noreply.github.com>
2020-06-17 11:00:29 -0500

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,11 +1,10 @@
 /* How to survive
- * Is it a dry place? We need fire and water first
+ * fire, water, and shelter then food 
+ * create fire and shelter after locating a water source
+ *once fire wanter and shelter are taken care of go find food 
  * ...
  */
  fn survive() {
-     fire 
-     water 
-     shelter
-     food
+     
     
  }
